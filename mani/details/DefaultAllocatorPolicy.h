edee1f986be97d915d18672abe3a195e1ade4b54
Dmitry Vedenko <vedenko@gmail.com>
2013-01-07 23:53:58 +0400

    DefaultAllocatorPolicy has been fixed

--- mani/details/DefaultAllocatorPolicy.h
+++ mani/details/DefaultAllocatorPolicy.h
@@ -1,3 +1,16 @@
+﻿//  ==================================================================================
+//
+//  File:        DefaultAllocatorPolicy.h
+//  Description:
+//  Comments:
+//  Author:      Dmitry Vedenko
+//  E-mail:      vedenko@gmail.com
+//
+//  The source code is licensed under the BSD 2-clause license. 
+//  Redistribution and use in source and binary forms, with or without modification, 
+//  are permitted provided that the requirements in the LICENSE file provided are met. 
+//
+//  ==================================================================================
 #ifndef DefaultAllocatorPolicy_h__
 #define DefaultAllocatorPolicy_h__
 
@@ -10,9 +23,9 @@ namespace mani
 		class DefaultAllocationPolicy
 		{
 		public:
-			void*     allocate( size_t size ) {	return ::malloc( size ); }
-			void*     reallocate( void* ptr, size_t old_size, size_t new_size ) { (void) old_size; return ::realloc( ptr, new_size ); }
-			void      deallocate( void* ptr, size_t size ) { (void) size; ::free( ptr ); }
+			static void*     allocate( size_t size ) {	return ::malloc( size ); }
+			static void*     reallocate( void* ptr, size_t old_size, size_t new_size ) { (void) old_size; return ::realloc( ptr, new_size ); }
+			static void      deallocate( void* ptr, size_t size ) { (void) size; ::free( ptr ); }
 		};
 	}
 }
